24d53b0cd86e640ea9840d03a86d24218379ad8c
exochron <exochron@freenet.de>
2024-12-23 11:20:13 +0100

    feat: preload game data into cache

--- core.lua
+++ core.lua
@@ -4,9 +4,19 @@ ADDON.Events = CreateFromMixins(EventRegistry)
 ADDON.Events:OnLoad()
 ADDON.Events:SetUndefinedEventsAllowed(true)
 
+local function loadDataIntoCache()
+    for _, row in ipairs(ADDON.db) do
+        if row.toy then
+            C_Item.RequestLoadItemDataByID(row.toy)
+        elseif row.item then
+            C_Item.RequestLoadItemDataByID(row.item)
+        end
+    end
+end
 
 ADDON.Events:RegisterFrameEventAndCallback("PLAYER_ENTERING_WORLD", function(_, isLogin, isReload)
     if isLogin or isReload then
+        loadDataIntoCache()
         ADDON.Events:TriggerEvent("OnInit")
         ADDON.Events:TriggerEvent("OnLogin")
         ADDON.Events:UnregisterEvents({"OnInit", "OnLogin"})
