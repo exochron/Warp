82dcb5f2c87c0d1b1716f23b5e26b1c5bc441884
exochron <exochron@freenet.de>
2025-01-06 14:48:50 +0100

    feat: added screenshot

---
+++ DevZone/Screenshot.lua
@@ -0,0 +1,17 @@
+local _, ADDON = ...
+
+function Scotty_TakeScreenshots()
+
+    local gg = LibStub("GalleryGenerator")
+    gg:TakeScreenshots(
+        {
+            function(api)
+                api:BackScreen()
+                ADDON:OpenTeleportMenu(BazookaPlugin_Scotty)
+
+                local menuChildren = { Menu.GetManager():GetOpenMenu():GetChildren() }
+                api:Point(menuChildren[18]) -- Broken Isles
+            end,
+        }
+    )
+end
